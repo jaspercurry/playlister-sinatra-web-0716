85458302450270de6a75b6168d186f5fc548c408
jaspercurry <jaspercurry@gmail.com>
2016-08-15 19:30:48 -0400

    this is broken

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,25 @@ class Artist <ActiveRecord::Base
   # has_many :song_genres
   has_many :genres, through: :songs
 
+  def slug
+    self.name.slugify
+  end
+
+  def self.find_by_slug(slug)
+    slug = slug.split("-")
+
+    caps = slug.map do |item|
+      item.capatalize
+    end
+
+  end
+
+  #   Artist.find_by(name: )
+  #
+  # describe "Class methods" do
+  #   it "given the slug can find an Artist" do
+  #     slug = "taylor-swift"
+  #     expect((Artist.find_by_slug(slug)).name).to eq("Taylor Swift")
+  #   end
+
 end
